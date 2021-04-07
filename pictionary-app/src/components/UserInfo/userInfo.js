6176c28fa3495520044d1541e783ec0f520f7961
Arp-G <arpanghoshal3@gmail.com>
2021-04-08 01:59:07 +0530

    add random avatar generator

--- pictionary-app/src/components/UserInfo/userInfo.js
+++ pictionary-app/src/components/UserInfo/userInfo.js
@@ -1,11 +1,15 @@
 import React from 'react';
 import { Card } from 'react-bootstrap';
+import RandomAvatar from '../randomAvatar/randomAvatar';
 
 const UserInfo = () => {
     return (
         <div>
             <Card>
-                <Card.Body>This is some text within a card body.</Card.Body>
+                <Card.Body>
+                    This is some text within a card body.
+                    <RandomAvatar />
+                    </Card.Body>
             </Card>
         </div>
     );
