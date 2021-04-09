21b31c199d1768250f1e073f204e88b7a1cf0ef6
Arp-G <arpanghoshal3@gmail.com>
2021-04-10 01:03:08 +0530

    improved visuals

--- pictionary-app/src/components/UserInfo/userInfo.js
+++ pictionary-app/src/components/UserInfo/userInfo.js
@@ -1,15 +1,42 @@
 import React from 'react';
-import { Card } from '@material-ui/core';
+import { Container, TextField, ButtonGroup, Button } from '@material-ui/core';
 import RandomAvatar from '../randomAvatar/randomAvatar';
+import { GiPerspectiveDiceSixFacesRandom } from 'react-icons/gi';
+import { FaUserEdit, FaPlay } from 'react-icons/fa';
+import { BsHouseFill } from 'react-icons/bs';
+import { useState } from 'react';
+import "./userInfo.css";
 
 const UserInfo = () => {
+
+    const [random, setRandom] = useState(Math.random());
+
     return (
-        <div>
-            <Card>
-                This is some text within a card body.
-                <RandomAvatar />
-            </Card>
-        </div>
+        <Container>
+            <TextField
+                label="Name"
+                placeholder="Your name here"
+                fullWidth={true}
+                variant="outlined"
+            />
+            <div>
+                <GiPerspectiveDiceSixFacesRandom
+                    size={'1.5em'}
+                    className={'randomAvatarIcon'}
+                    onClick={() => setRandom(Math.random())}
+                />
+                <FaUserEdit
+                    size={'1.2em'}
+                    className={'avatarSettingsIcon'}
+                    onClick={() => setRandom(Math.random())}
+                />
+                <RandomAvatar random={random} />
+                <ButtonGroup variant={'contained'} fullWidth={true} style={{ marginTop: '15px' }}>
+                    <Button startIcon={<FaPlay />} style={{ backgroundColor: '#228b22', color: 'white' }}> Play ! </Button>
+                    <Button startIcon={<BsHouseFill />} color='primary'> Create Private Room </Button>
+                </ButtonGroup>
+            </div>
+        </Container>
     );
 }
 
--- pictionary-app/src/pages/Home.js
+++ pictionary-app/src/pages/Home.js
@@ -3,14 +3,22 @@ import Header from '../components/Header/header';
 import UserInfo from '../components/UserInfo/userInfo';
 import HowToPlay from '../components/HowToPlay/howToPlay';
 import About from '../components/About/about';
+import { Container, Paper } from '@material-ui/core'
+import './home.css';
 
 const Home = () => {
-    return (<div>
-        <Header />
-        <UserInfo />
-        <HowToPlay />
-        <About />
-    </div>);
+    return (
+        <Container maxWidth={'sm'} alignContent={'center'}>
+            <Header />
+            <Paper className={'wrapped-paper'}>
+                <UserInfo />
+            </Paper>
+            <Paper className={'wrapped-paper'}>
+                <HowToPlay />
+                <About />
+            </Paper>
+        </Container>
+    );
 }
 
 export default Home;
