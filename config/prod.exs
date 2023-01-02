e3b7fc6e0c78712c80cc5b279860215726cb8ffd
Arp <arpanghoshal3@gmail.com>
2023-01-02 23:19:03 +0530

    remove uneeded cache manifest

--- config/prod.exs
+++ config/prod.exs
@@ -1,8 +1,7 @@
 import Config
 
 config :pictionary, PictionaryWeb.Endpoint,
-  url: [host: "example.com", port: 80],
-  cache_static_manifest: "priv/static/cache_manifest.json"
+  url: [host: "example.com", port: 80]
 
 # Do not print debug messages in production
 config :logger, level: :info
